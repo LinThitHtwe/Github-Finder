5a39f0365eb528fc2b398a498f6f10916db0dfe9
LinThit27 <linthit2745@gmail.com>
2024-02-26 09:48:50 +0630

    [add] metadata and favicon

--- src/api/fetchMethods.ts
+++ src/api/fetchMethods.ts
@@ -1,16 +1,17 @@
 import { checkResponseStatus } from "@/errors/checkResponseStatus";
 
+const BASEURL = "https://api.github.com/users";
+const BASEQUERY = "per_page=150";
+
 export const fetchRepoData = async (username: string) => {
-  const response = await fetch(
-    `https://api.github.com/users/${username}/repos?per_page=1000`,
-  );
+  const response = await fetch(`${BASEURL}/${username}/repos?${BASEQUERY}`);
   if (!response.ok) checkResponseStatus(response.status);
   const data = await response.json();
   return data;
 };
 
 export const fetchUserData = async (username: string) => {
-  const response = await fetch(`https://api.github.com/users/${username}`);
+  const response = await fetch(`${BASEURL}/${username}`);
   if (!response.ok) checkResponseStatus(response.status);
   const data = await response.json();
   return data;
@@ -18,7 +19,7 @@ export const fetchUserData = async (username: string) => {
 
 export const fetchStarredData = async (username: string) => {
   const response = await fetch(
-    `https://api.github.com/users/${username}/starred?per_page=10000`,
+    `${BASEURL}/${username}/starred?per_page=${BASEQUERY}`,
   );
   if (!response.ok) checkResponseStatus(response.status);
   const data = await response.json();
@@ -26,18 +27,14 @@ export const fetchStarredData = async (username: string) => {
 };
 
 export const fetchFollowersData = async (username: string) => {
-  const response = await fetch(
-    `https://api.github.com/users/${username}/followers?per_page=100000`,
-  );
+  const response = await fetch(`${BASEURL}/${username}/followers?${BASEQUERY}`);
   if (!response.ok) checkResponseStatus(response.status);
   const data = await response.json();
   return data;
 };
 
 export const fetchFollowingData = async (username: string) => {
-  const response = await fetch(
-    `https://api.github.com/users/${username}/following?per_page=100000`,
-  );
+  const response = await fetch(`${BASEURL}/${username}/following?${BASEQUERY}`);
   if (!response.ok) checkResponseStatus(response.status);
   const data = await response.json();
   return data;
